ffdfd23f4df379c7ba09c6655841143cf98945b7
Clément Laigle <c.laigle@catie.fr>
2024-11-25 16:48:14 +0100

    Remove custom revision.cmake file from board template

--- core/{{cookiecutter.project_slug}}/boards/{{cookiecutter.__board_vendor_lower}}/{{cookiecutter.__board_name_lower}}/revision.cmake
+++
@@ -1,8 +0,0 @@
-# Copyright (c) {{cookiecutter.copyright_year}} {{cookiecutter.copyright_holder}}
-# SPDX-License-Identifier: Apache-2.0
-
-board_check_revision(
-  FORMAT MAJOR.MINOR.PATCH
-  DEFAULT_REVISION {{cookiecutter.board_default_version}}
-  VALID_REVISIONS {{cookiecutter.board_default_version}}
-)
