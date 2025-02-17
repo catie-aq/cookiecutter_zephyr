04121d2a97e9ec618aa196875c45a2840a2e47ff
l_saintgermain_catie <l.saintgermain@catie.fr>
2025-02-17 10:35:57 +0100

    Added catie_slug cookiecutter parameter

--- sensor/hooks/post_gen_project.py
+++ sensor/hooks/post_gen_project.py
@@ -37,7 +37,7 @@ def main():
     print("Add these lines to your manifest file:")
     print(
         f"- name: {{cookiecutter.project_slug}}\n"
-        f"    repo-path: zephyr_{{cookiecutter.project_slug}}\n"
+        f"    repo-path: zephyr_{{cookiecutter.catie_slug}}\n"
         f"    revision: main\n"
         f"    path: 6tron/drivers/{{cookiecutter.project_slug}}\n"
     )
