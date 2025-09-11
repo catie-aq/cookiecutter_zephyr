94c8cfc32e25c9587c42973642fee0c07d09cc1d
Benjamin Grolleau <127044450+Grolleau-Benjamin@users.noreply.github.com>
2025-09-11 09:57:06 +0200

    Add support for secure application with STSafe configuration and cleanup

---
+++ workspace_application/{{cookiecutter.project_slug}}/README.md
@@ -0,0 +1,13 @@
+# {{cookiecutter.application}}
+
+{% if cookiecutter.secure_app|lower == "true" -%}
+
+> [!WARNING]
+>
+> Depending on which resistor you’ve fitted on your Zest Security Secure Element, you must update `app.overlay` accordingly:
+>
+> - R3 (Default): `<&sixtron_connector DIO1 GPIO_ACTIVE_HIGH>;`
+> - R4: `<&sixtron_connector DIO6 GPIO_ACTIVE_HIGH>;`
+> - R5: `<&sixtron_connector DIO11 GPIO_ACTIVE_HIGH>;`
+
+{% endif -%}
--- workspace_application/{{cookiecutter.project_slug}}/app/src/main.c
+++ workspace_application/{{cookiecutter.project_slug}}/app/src/main.c
@@ -5,8 +5,41 @@
 
 #include <zephyr/kernel.h>
 
+{% if cookiecutter.secure_app|lower == "true" -%}
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+
+#include "stselib.h"
+
+const struct gpio_dt_spec stsafe_reset = GPIO_DT_SPEC_GET(ZEPHYR_USER_NODE, stsafereset_gpios);
+{% endif -%}
+
 int main(void)
 {
+{% if cookiecutter.secure_app|lower == "true" -%}
+	int ret = STSE_OK;
+	if (!gpio_is_ready_dt(&stsafe_reset)) {
+		printk("STSafe reset GPIO is not ready!");
+		return -1;
+	}
+	if (gpio_pin_configure_dt(&stsafe_reset, GPIO_OUTPUT_ACTIVE) < 0) {
+		printk("Failed to configure STSafe reset GPIO!");
+		return -1;
+	}
+
+	#ifdef CONFIG_STSAFE_A110
+		stse_handler.device_type = STSAFE_A110;
+	#elif defined(CONFIG_STSAFE_A120)
+		stse_handler.device_type = STSAFE_A120;
+	#endif
+
+	ret = stse_init(&stse_handler);
+	if (ret != STSE_OK) {
+		printk("Failed to initialize STSafe handler: %d", ret);
+		return -1;
+	}
+{% endif -%}
+
 	while (1) {
 		printk("Alive\n");
 		k_sleep(K_MSEC(1000));
