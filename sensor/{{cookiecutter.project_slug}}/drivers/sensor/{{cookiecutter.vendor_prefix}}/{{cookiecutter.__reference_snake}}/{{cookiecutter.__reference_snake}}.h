ccfa1fc60356da719040ed721ae15293c78a5511
l_saintgermain_catie <l.saintgermain@catie.fr>
2025-10-22 15:40:19 +0200

    feat: add bus structs

--- sensor/{{cookiecutter.project_slug}}/drivers/sensor/{{cookiecutter.vendor_prefix}}/{{cookiecutter.__reference_snake}}/{{cookiecutter.__reference_snake}}.h
+++ sensor/{{cookiecutter.project_slug}}/drivers/sensor/{{cookiecutter.vendor_prefix}}/{{cookiecutter.__reference_snake}}/{{cookiecutter.__reference_snake}}.h
@@ -5,8 +5,21 @@
 
 #ifndef ZEPHYR_DRIVERS_SENSOR_{{cookiecutter.__reference_snake.upper()}}_{{cookiecutter.__reference_snake.upper()}}_H_
 #define ZEPHYR_DRIVERS_SENSOR_{{cookiecutter.__reference_snake.upper()}}_{{cookiecutter.__reference_snake.upper()}}_H_
-
+{% if cookiecutter.bus == "I2C" %}
+#include <zephyr/drivers/i2c.h>
+{% elif cookiecutter.bus == "SPI" %}
+#include <zephyr/drivers/spi.h>
+{% elif cookiecutter.bus == "SERIAL" %}
+#include <zephyr/drivers/uart.h>
+{% endif %}
 struct {{cookiecutter.__reference_snake}}_config {
+{%- if cookiecutter.bus == "I2C" %}
+    struct i2c_dt_spec i2c;
+{%- elif cookiecutter.bus == "SPI" %}
+    struct spi_dt_spec spi;
+{%- elif cookiecutter.bus == "SERIAL" %}
+    const struct device *uart;
+{%- endif %}
 };
 
 struct {{cookiecutter.__reference_snake}}_data {
