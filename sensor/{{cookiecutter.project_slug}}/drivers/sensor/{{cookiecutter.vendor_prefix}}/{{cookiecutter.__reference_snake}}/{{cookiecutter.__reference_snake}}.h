41d2f0a1d62a776c95ef9c74e957432202400397
l_saintgermain_catie <l.saintgermain@catie.fr>
2025-10-22 15:09:34 +0200

    feat: change config/data struct location

--- sensor/{{cookiecutter.project_slug}}/drivers/sensor/{{cookiecutter.vendor_prefix}}/{{cookiecutter.__reference_snake}}/{{cookiecutter.__reference_snake}}.h
+++ sensor/{{cookiecutter.project_slug}}/drivers/sensor/{{cookiecutter.vendor_prefix}}/{{cookiecutter.__reference_snake}}/{{cookiecutter.__reference_snake}}.h
@@ -6,4 +6,10 @@
 #ifndef ZEPHYR_DRIVERS_SENSOR_{{cookiecutter.__reference_snake.upper()}}_{{cookiecutter.__reference_snake.upper()}}_H_
 #define ZEPHYR_DRIVERS_SENSOR_{{cookiecutter.__reference_snake.upper()}}_{{cookiecutter.__reference_snake.upper()}}_H_
 
+struct {{cookiecutter.__reference_snake}}_config {
+};
+
+struct {{cookiecutter.__reference_snake}}_data {
+};
+
 #endif /* ZEPHYR_DRIVERS_SENSOR_{{cookiecutter.__reference_snake.upper()}}_{{cookiecutter.__reference_snake.upper()}}_H_ */
